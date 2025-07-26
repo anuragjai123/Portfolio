45eae64737ac6702f39d1a67f3c31091ef702e27
anuragjai123 <jaiswalanurag848@gmail.com>
2025-07-26 10:48:13 +0530

    changes are done

--- resume-website/script.js
+++ resume-website/script.js
@@ -10,3 +10,12 @@ function closeModal(id) {
 document.getElementById('theme-toggle').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
 });
+
+  function showSidebar() {
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'flex'
+        }
+        function hideSidebar() {
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'none'
+        }
